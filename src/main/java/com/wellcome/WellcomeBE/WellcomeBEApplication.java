3fe154c308ecac2d94abf92bf3595cf575227e31
eujjong18 <eujjong18@naver.com>
2024-06-19 01:32:26 +0900

    initial commit

---
+++ src/main/java/com/wellcome/WellcomeBE/WellcomeBEApplication.java
@@ -0,0 +1,13 @@
+package com.wellcome.WellcomeBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellcomeBEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WellcomeBEApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wellcome/WellcomeBE/WellcomeBEApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellcome.WellcomeBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellcomeBEApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
