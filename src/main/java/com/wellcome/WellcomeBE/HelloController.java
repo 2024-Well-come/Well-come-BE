7e6e3f34fb8f1d0c4e6b44c51d3d5998112e7129
hyesoo kim <khwater1017@naver.com>
2024-07-18 01:15:35 +0900

    [test] 배포 테스트

--- src/main/java/com/wellcome/WellcomeBE/HelloController.java
+++ src/main/java/com/wellcome/WellcomeBE/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    // jar 빌드 테스트용 
     @GetMapping("/health-check")
     public String hello(){
         return "health-check";
