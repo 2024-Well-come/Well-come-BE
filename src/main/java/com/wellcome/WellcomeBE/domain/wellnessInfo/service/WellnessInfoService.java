b5fda2b67bde2100fdc68e1d2dc613f101d5aaab
eujjong18 <eujjong18@naver.com>
2024-08-04 02:11:08 +0900

    [feat #13] 한국관광공사 국문 관광정보 API 호출 로직 추가 (키워드 검색조회) 및 필터링 할 키워드 리스트 추가

--- src/main/java/com/wellcome/WellcomeBE/domain/wellnessInfo/service/WellnessInfoService.java
+++ src/main/java/com/wellcome/WellcomeBE/domain/wellnessInfo/service/WellnessInfoService.java
@@ -8,40 +8,62 @@ import com.wellcome.WellcomeBE.global.type.Thema;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.locationtech.jts.io.ParseException;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.List;
+import static com.wellcome.WellcomeBE.global.type.Keyword.KEYWORDS;
 
 @Service
-@RequiredArgsConstructor
 @Slf4j
 public class WellnessInfoService {
 
+    @Qualifier("tourBasicApiWebClient")
     private final WebClient tourBasicApiWebClient;
+    @Qualifier("tourSearchApiWebClient")
+    private final WebClient tourSearchApiWebClient;
     private final WellnessInfoRepository wellnessInfoRepository;
 
     private static final String GANGWONDO_AREACODE = "32";
-    private static final int NUM_OF_ROWS = 100;
+    private static final int NUM_OF_ROWS_BASIC = 100;
+    private static final int NUM_OF_ROWS_SEARCH = 5;
+
+    public WellnessInfoService(
+            @Qualifier("tourBasicApiWebClient") WebClient tourBasicApiWebClient,
+            @Qualifier("tourSearchApiWebClient") WebClient tourSearchApiWebClient,
+            WellnessInfoRepository wellnessInfoRepository) {
+        this.tourBasicApiWebClient = tourBasicApiWebClient;
+        this.tourSearchApiWebClient = tourSearchApiWebClient;
+        this.wellnessInfoRepository = wellnessInfoRepository;
+    }
 
     public void fetchAndSaveTourInfo() {
+
+        // Basic API 호출 및 저장
         Flux.fromIterable(CategoryDetail.CATEGORY_PARAM_LIST)
-                .flatMap(categoryDetail -> fetchData(categoryDetail, 1))
+                .flatMap(this::fetchData)
                 .buffer(50)
                 .doOnNext(entities -> wellnessInfoRepository.saveAll(entities))
                 .collectList()
                 .block();
+
+        // Search API 호출 및 저장
+        Flux.fromIterable(KEYWORDS)
+                .flatMap(this::fetchDataByKeyword)
+                .collectList()
+                .doOnNext(entities -> wellnessInfoRepository.saveAll(entities))
+                .block();
     }
 
-    private Flux<WellnessInfo> fetchData(CategoryDetail categoryDetail, int pageNo) {
+    private Flux<WellnessInfo> fetchData(CategoryDetail categoryDetail) {
         return fetchFromTourBasicApi(categoryDetail, 1)
                 .expand(response -> {
                     int currentPage = response.getResponse().getBody().getPageNo();
                     int totalCount = response.getResponse().getBody().getTotalCount();
 
-                    if (currentPage * NUM_OF_ROWS < totalCount) {
+                    if (currentPage * NUM_OF_ROWS_BASIC < totalCount) {
                         return fetchFromTourBasicApi(categoryDetail, currentPage + 1);
                     } else {
                         return Mono.empty();
@@ -62,7 +84,7 @@ public class WellnessInfoService {
                 .uri(uriBuilder -> {
                     uriBuilder.queryParam("areaCode", GANGWONDO_AREACODE)
                             .queryParam("pageNo", pageNo)
-                            .queryParam("numOfRows", NUM_OF_ROWS)
+                            .queryParam("numOfRows", NUM_OF_ROWS_BASIC)
                             .queryParam("cat1", categoryDetail.getCat1());
 
                     if (categoryDetail.getCat2() != null) {
@@ -73,6 +95,41 @@ public class WellnessInfoService {
                     }
                     return uriBuilder.build();
                 })
+                .retrieve()
+                .bodyToMono(TourBasicApiResponse.class);
+    }
+
+    private Flux<WellnessInfo> fetchDataByKeyword(String keyword) {
+        return fetchDataFromTourSearchApi(keyword, 1)
+                .expand(response -> {
+                    int currentPage = response.getResponse().getBody().getPageNo();
+                    int totalCount = response.getResponse().getBody().getTotalCount();
+
+                    if (currentPage * NUM_OF_ROWS_SEARCH < totalCount) {
+                        return fetchDataFromTourSearchApi(keyword, currentPage + 1);
+                    } else {
+                        return Mono.empty();
+                    }
+                })
+                .flatMap(response -> {
+                    TourBasicApiResponse.Response.Body.Items items = response.getResponse().getBody().getItems();
+                    if (items == null || items.getItem() == null || items.getItem().isEmpty()) {
+                        log.info("keyword '{}'에 대한 검색 결과 없음", keyword);
+                        return Flux.empty();
+                    }
+                    return Flux.fromIterable(response.getResponse().getBody().getItems().getItem());
+                })
+                .map(this::convertToEntity);
+    }
+
+    private Mono<TourBasicApiResponse> fetchDataFromTourSearchApi(String keyword, int pageNo) {
+        return tourSearchApiWebClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .queryParam("areaCode", GANGWONDO_AREACODE)
+                        .queryParam("pageNo", pageNo)
+                        .queryParam("numOfRows", NUM_OF_ROWS_SEARCH)
+                        .queryParam("keyword", keyword)
+                        .build())
                 .retrieve()
                 .bodyToMono(TourBasicApiResponse.class);
     }
