3de30d2fc770488a256adf8400010b0d53a23ced
eujjong18 <eujjong18@naver.com>
2024-08-13 21:02:26 +0900

    [refactor #16] 한국관광공사 국문 관광정보 API WebClient 설정: 공통 URL 생성 로직 메소드로 분리

--- src/main/java/com/wellcome/WellcomeBE/global/config/TourInfoApiWebClientConfig.java
+++ src/main/java/com/wellcome/WellcomeBE/global/config/TourInfoApiWebClientConfig.java
@@ -1,26 +1,40 @@
 package com.wellcome.WellcomeBE.global.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Configuration
+@Slf4j
 public class TourInfoApiWebClientConfig {
 
     @Value("${api.key}")
     private String apiKey;
 
+    private static final String BASE_URL = "http://apis.data.go.kr/B551011/KorService1/";
+
+    // 공통 baseUrl 생성
+    private String buildUrl(String endpoint) {
+        String uriString = UriComponentsBuilder.fromHttpUrl(BASE_URL + endpoint)
+                .queryParam("serviceKey", apiKey)
+                .queryParam("MobileOS", "AND")
+                .queryParam("MobileApp", "Wellcome")
+                .queryParam("_type", "json")
+                .queryParam("listYN", "Y")
+                .build(false)
+                .toUriString();
+
+        return uriString;
+    }
+
     // 한국관광공사_국문 관광정보 서비스 API - 지역기반관광정보조회
     @Bean
     public WebClient tourBasicApiWebClient() {
         return WebClient.builder()
-                .baseUrl("http://apis.data.go.kr/B551011/KorService1/areaBasedList1?"
-                        + "serviceKey=" + apiKey
-                        + "&MobileOS=AND"
-                        + "&MobileApp=Wellcome"
-                        + "&_type=json"
-                        + "&listYN=Y")
+                .baseUrl(buildUrl("areaBasedList1"))
                 .build();
     }
 
@@ -28,12 +42,7 @@ public class TourInfoApiWebClientConfig {
     @Bean
     public WebClient tourSearchApiWebClient() {
         return WebClient.builder()
-                .baseUrl("http://apis.data.go.kr/B551011/KorService1/searchKeyword1?"
-                        + "serviceKey=" + apiKey
-                        + "&MobileOS=AND"
-                        + "&MobileApp=Wellcome"
-                        + "&_type=json"
-                        + "&listYN=Y")
+                .baseUrl(buildUrl("searchKeyword1"))
                 .build();
     }
 
