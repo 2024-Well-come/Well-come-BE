3409490a2a72abd3f0f66c654356231fd20afe76
eujjong18 <eujjong18@naver.com>
2024-08-24 20:53:05 +0900

    [feat #19] 토큰 갱신 로직 구현 - feat: access token 만료 시, 갱신 로직 구현 - fix: 회원가입 로직 수정(Member 저장 로직 추가)

--- src/main/java/com/wellcome/WellcomeBE/domain/member/MemberController.java
+++ src/main/java/com/wellcome/WellcomeBE/domain/member/MemberController.java
@@ -4,24 +4,30 @@ import com.wellcome.WellcomeBE.domain.member.dto.response.KakaoTokenResponse;
 import com.wellcome.WellcomeBE.domain.member.dto.response.KakaoUserInfoResponse;
 import com.wellcome.WellcomeBE.domain.member.dto.response.LoginResponse;
 import com.wellcome.WellcomeBE.domain.member.service.KakaoAuthService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping("/api/oauth2/kakao")
 public class MemberController {
 
     private final KakaoAuthService kakaoService;
 
     // 카카오 로그인
-    @GetMapping("/api/oauth2/kakao")
+    @GetMapping
     public ResponseEntity<LoginResponse> kakaoLogin(@RequestParam("code") String code){
         return ResponseEntity.ok(kakaoService.handleKakaoLogin(code));
+    }
+
+    // 토큰 갱신
+    @PostMapping("/refresh")
+    public ResponseEntity<LoginResponse> kakaoRenewToken(HttpServletRequest httpServletRequest){
+        return ResponseEntity.ok(kakaoService.renewKakaoToken(httpServletRequest));
     }
 
 }
--- src/main/java/com/wellcome/WellcomeBE/domain/member/service/KakaoAuthService.java
+++ src/main/java/com/wellcome/WellcomeBE/domain/member/service/KakaoAuthService.java
@@ -6,10 +6,12 @@ import com.wellcome.WellcomeBE.domain.member.dto.response.KakaoTokenResponse;
 import com.wellcome.WellcomeBE.domain.member.dto.response.KakaoUserInfoResponse;
 import com.wellcome.WellcomeBE.domain.member.dto.response.LoginResponse;
 import com.wellcome.WellcomeBE.global.type.SocialLogin;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
@@ -59,7 +61,7 @@ public class KakaoAuthService {
         return new LoginResponse(tokenResponse.getAccessToken(), tokenResponse.getRefreshToken());
     }
 
-    // 토큰 받기
+    // 카카오 API 호출 - 토큰 받기
     private Mono<KakaoTokenResponse> getAccessToken(String code) {
         String requestUrl = UriComponentsBuilder
                 .fromUriString("https://kauth.kakao.com/oauth/token")
@@ -78,7 +80,7 @@ public class KakaoAuthService {
                 .bodyToMono(KakaoTokenResponse.class);
     }
 
-    // 사용자 정보 가져오기
+    // 카카오 API 호출 - 사용자 정보 가져오기
     private Mono<KakaoUserInfoResponse> getUserInfo(String accessToken) {
         String requestUrl = UriComponentsBuilder
                 .fromUriString("https://kapi.kakao.com/v2/user/me")
@@ -94,22 +96,86 @@ public class KakaoAuthService {
                 .bodyToMono(KakaoUserInfoResponse.class);
     }
 
+    // 카카오 API 호출 - 토큰 갱신하기
+    private Mono<KakaoTokenResponse> getNewToken(String refreshToken) {
+        String requestUrl = UriComponentsBuilder
+                .fromUriString("https://kauth.kakao.com/oauth/token")
+                .queryParam("grant_type", "refresh_token")
+                .queryParam("client_id", clientId)
+                .queryParam("refresh_token", refreshToken)
+                .build()
+                .toUriString();
+
+        return webClient.post()
+                .uri(requestUrl)
+                .header("Content-Type", "application/x-www-form-urlencoded;charset=utf-8")
+                .retrieve()
+                .bodyToMono(KakaoTokenResponse.class);
+    }
+
     // 회원가입 여부 확인 후 회원가입 진행
     private void verifyAndRegisterMember(KakaoUserInfoResponse userInfoResponse) {
         Optional<Member> member = memberRepository.findByKakaoId(userInfoResponse.getId());
         if(!member.isPresent()){
-            Member.createKakaoUser(userInfoResponse);
+            memberRepository.save(Member.createKakaoUser(userInfoResponse));
         }
     }
 
     // refresh token 저장
     private void saveRefreshToken(Long kakaoId, String refreshToken, Integer expiresIn) {
-        redisTemplate.opsForValue().set(String.valueOf(kakaoId), refreshToken, Long.valueOf(expiresIn));
+        // key: refreshToken, value: kakaoId
+        redisTemplate.opsForValue().set(refreshToken, String.valueOf(kakaoId), Long.valueOf(expiresIn));
     }
 
     // refresh token 조회
-    private String getRefreshToken(Long kakaoId) {
-        return (String) redisTemplate.opsForValue().get(String.valueOf(kakaoId));
+    private String getRefreshToken(String refreshToken) {
+        return (String) redisTemplate.opsForValue().get(refreshToken);
+    }
+
+    /**
+     * 토큰 갱신
+     */
+    public LoginResponse renewKakaoToken(HttpServletRequest httpServletRequest) {
+
+        // 헤더에서 refresh token 추출
+        String refreshToken = extractToken(httpServletRequest);
+
+        // refresh token 유효성 확인
+        if(getRefreshToken(refreshToken) == null) { //refresh token 만료
+            // TODO Custom Exception 처리: 재로그인 요청
+        }
+
+        // 토큰 재발급 요청
+        KakaoTokenResponse newTokenResponse = getNewToken(refreshToken).block();
+        Long kakaoId = getUserInfo(newTokenResponse.getAccessToken()).block().getId();
+
+        // refresh token도 갱신된 경우 업데이트
+        if(newTokenResponse.getRefreshToken() != null){
+            updateRefreshToken(kakaoId, refreshToken, newTokenResponse);
+            refreshToken = newTokenResponse.getRefreshToken();
+        }
+
+        // 갱신된 access token, refresh token 응답
+        return new LoginResponse(newTokenResponse.getAccessToken(), refreshToken);
+    }
+
+    // request header에서 token 추출
+    private String extractToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    // refresh token이 갱신된 경우 업데이트
+    private String updateRefreshToken(Long kakaoId, String oldRefreshToken, KakaoTokenResponse newTokenResponse){
+        String newRefreshToken = newTokenResponse.getRefreshToken();
+
+        redisTemplate.delete(oldRefreshToken);
+        saveRefreshToken(kakaoId, newRefreshToken, newTokenResponse.getRefreshTokenExpiresIn());
+
+        return newRefreshToken;
     }
 
 }
