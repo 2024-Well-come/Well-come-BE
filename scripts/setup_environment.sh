27434ffe3994556c4cc42555fb7b3334c8c1b33b
hyesoo kim <khwater1017@naver.com>
2024-09-05 02:16:58 +0900

    [ci/cd #1] 스크립트 환경 변수 적용

--- scripts/setup_environment.sh
+++ scripts/setup_environment.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 # 환경 변수 파일 설정
-sudo cp /home/ubuntu/app/config/wellcome_env.sh /etc/profile.d/
-sudo chmod +x /etc/profile.d/wellcome_env.sh
-source /etc/profile
+source ~/.bash_profile
 
 # PEM 키 설정
-sudo cp /home/ubuntu/app/config/2024wellcome.pem /home/ubuntu/
-sudo chmod 400 /home/ubuntu/2024wellcome.pem
+sudo chmod 400 ${SSH_PRIVATE_KEY_PATH}
 
 # SSH 터널링 설정 스크립트 생성
 cat <<EOT | sudo tee /etc/systemd/system/ssh-tunnel.service
@@ -16,7 +13,7 @@ Description=SSH Tunnel for RDS
 After=network.target
 
 [Service]
-ExecStart=/usr/bin/ssh -i /home/ubuntu/2024wellcome.pem -N -L 18084:db-wellcome.clwgsqaykgi6.ap-northeast-2.rds.amazonaws.com:3306 ubuntu@13.124.241.200
+ExecStart=/usr/bin/ssh -i ${SSH_PRIVATE_KEY_PATH} -N -L ${SSH_LOCAL_PORT}:${DB_ENDPOINT}:${DB_PORT} ${SSH_USER}@${SSH_REMOTE_JUMP_HOST}
 Restart=always
 User=ubuntu
 
