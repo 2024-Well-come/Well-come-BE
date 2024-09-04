75915b3b71fd57169565504e0f6966ea95e1636e
eujjong18 <eujjong18@naver.com>
2024-09-04 16:21:57 +0900

    [feat #34] 여행 폴더 생성 및 여행지 추가 API - 응답 코드 201 -> 200으로 변경

--- src/main/java/com/wellcome/WellcomeBE/domain/tripPlan/controller/TripPlanController.java
+++ src/main/java/com/wellcome/WellcomeBE/domain/tripPlan/controller/TripPlanController.java
@@ -19,23 +19,27 @@ public class TripPlanController {
     private final TripPlanService tripPlanService;
     private final TripPlanPlaceService tripPlanPlaceService;
 
+    // 여행 폴더 생성
     @PostMapping("/plans")
     public ResponseEntity<?> addTripPlan(@RequestBody TripPlanRequest request){
         tripPlanService.createTripPlan(request);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        return ResponseEntity.ok().build();
     }
 
+    // 여행 폴더 내 여행지 추가
     @PostMapping("/plans/{planId}/places")
     public ResponseEntity<?> addTripPlanPlace(@PathVariable Long planId, @RequestBody TripPlanPlaceRequest request){
         tripPlanPlaceService.createTripPlanPlace(planId,request);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        return ResponseEntity.ok().build();
     }
 
+    // 여행 폴더 목록 조회
     @GetMapping("/plans/brief")
     public ResponseEntity<TripPlanResponse.TripPlanListResponse> tripPlanList(){
         return ResponseEntity.ok(tripPlanService.getTripPlanList());
     }
 
+    // 여행 폴더 삭제
     @DeleteMapping("/plans")
     public ResponseEntity<Void> deleteTripPlan(@RequestBody TripPlanDeleteRequest request){
         tripPlanService.deleteTripPlan(request);
