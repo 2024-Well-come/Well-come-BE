122425dc6d3d2987bb05a9cfbee04bc1f1dff39e
hyesoo kim <khwater1017@naver.com>
2024-09-08 02:55:40 +0900

    [CD #1] 부드러운 종료를 시도 후에도 미종료 시 강제 종료 추가
    CURRENT_PID가 비어 있는지 확인하고, 비어 있지 않으면 kill -15로 애플리케이션을 종료합니다.
    5초 후에도 애플리케이션이 종료되지 않으면 kill -9로 강제로 종료합니다.

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -15,9 +15,16 @@ chown ubuntu:ubuntu $DEPLOY_LOG
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 
 # 프로세스가 켜져 있으면 종료
-if [ -z $CURRENT_PID ]; then
-  echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
+if [ -z "$CURRENT_PID" ]; then
+  echo "$TIME_NOW > 현재 실행 중인 애플리케이션이 없습니다." >> $DEPLOY_LOG
 else
-  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
+  echo "$TIME_NOW > 실행 중인 애플리케이션 (PID: $CURRENT_PID) 종료 시도" >> $DEPLOY_LOG
   kill -15 $CURRENT_PID
+
+  # 프로세스가 종료되지 않은 경우 강제 종료
+  sleep 5
+  if pgrep -f $JAR_FILE > /dev/null; then
+    echo "$TIME_NOW > 프로세스가 여전히 실행 중입니다. 강제 종료합니다." >> $DEPLOY_LOG
+    kill -9 $CURRENT_PID
+  fi
 fi
