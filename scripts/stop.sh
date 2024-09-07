941381efd122607760da39058ce562f81434f7bd
hyesoo kim <khwater1017@naver.com>
2024-09-08 03:30:17 +0900

    [CD #1] JAR_FILE 경로 오류 수정

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PROJECT_ROOT="/home/ubuntu/spring-github-action"
-JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
+JAR_FILE="$PROJECT_ROOT/build/libs/WellcomeBE-0.0.1-SNAPSHOT.jar"
 
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
@@ -20,20 +20,4 @@ if [ -z "$CURRENT_PID" ]; then
 else
   echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료" >> $DEPLOY_LOG
   kill -15 $CURRENT_PID
-
-  # 애플리케이션이 완전히 종료될 때까지 대기
-  for i in {1..10}; do
-    CURRENT_PID=$(pgrep -f $JAR_FILE)
-    if [ -z "$CURRENT_PID" ]; then
-      echo "$TIME_NOW > 애플리케이션이 성공적으로 종료되었습니다." >> $DEPLOY_LOG
-      break
-    fi
-    echo "$TIME_NOW > 애플리케이션 종료 대기 중... ($i/10)" >> $DEPLOY_LOG
-    sleep 3
-  done
-
-  if [ -n "$CURRENT_PID" ]; then
-    echo "$TIME_NOW > 애플리케이션 종료 실패, 프로세스가 여전히 실행 중입니다." >> $DEPLOY_LOG
-    exit 1
-  fi
 fi
