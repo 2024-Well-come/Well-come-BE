d45efcd3ded2d35a652669bbc010298bbb8703e6
eujjong18 <eujjong18@naver.com>
2024-09-10 03:33:04 +0900

    [fix #23] request dto, response dto 여행 날짜 형식 YYYY.MM.dd 형식으로 통일

--- src/main/java/com/wellcome/WellcomeBE/domain/wellnessInfo/dto/request/WellnessInfoListRequest.java
+++ src/main/java/com/wellcome/WellcomeBE/domain/wellnessInfo/dto/request/WellnessInfoListRequest.java
@@ -1,9 +1,11 @@
 package com.wellcome.WellcomeBE.domain.wellnessInfo.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.wellcome.WellcomeBE.global.type.Sigungu;
 import com.wellcome.WellcomeBE.global.type.Thema;
 import lombok.Getter;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Getter
@@ -11,7 +13,11 @@ public class WellnessInfoListRequest {
 
     private List<Thema> themaList;
     private List<Sigungu> sigunguList;
-    private String tripStartDate;
-    private String tripEndDate;
+
+    @JsonFormat(pattern = "yyyy.MM.dd")
+    private LocalDate tripStartDate;
+
+    @JsonFormat(pattern = "yyyy.MM.dd")
+    private LocalDate tripEndDate;
 
 }
